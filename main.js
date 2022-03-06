024eb47a39c92d3e8460cba54128fb1268966d1c
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-03-06 22:04:02 +0200

    changed css html layout

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let form = document.querySelector("form");
 let color1 = document.querySelector("#color1");
 let color2 = document.querySelector("#color2");
-let direction = document.querySelectorAll("#direction");
+let direction = document.querySelectorAll(".nappi");
+let canvas = document.querySelector("#gradient")
+let css = document.querySelector("#css");
 
 const gradient = () => {
     let selectedDirection;
-    console.log(direction)
+    /**console.log(direction) */
 
     for (const selection of direction) {
         if (selection.checked) {
@@ -13,8 +15,8 @@ const gradient = () => {
         }
     }
 
-    document.body.style.backgroundImage = `linear-gradient(${selectedDirection}, ${color1.value}, ${color2.value})`
-
+    canvas.style.backgroundImage = `linear-gradient(${selectedDirection}, ${color1.value}, ${color2.value})`
+    css.textContent = `CSS: linear-gradient(${selectedDirection}, ${color1.value}, ${color2.value})`
 }
 
 form.addEventListener("change", gradient);
